5a39e2dc1d17da683496ec6c09fc8cac6bf0ce48
platisam <platisam@gmail.com>
2023-06-30 10:53:51 +0200

    added input and select element

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ function App() {
       <header className="App-header">
         <p>Currency Converter</p>
       </header>
+      <input type="text" />
+
+      <select>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+      </select>
+
+      <select>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+      </select>
     </div>
   );
 }
