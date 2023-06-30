d2667403078e3fa9c0564102c9b08c53e43b2928
platisam <platisam@gmail.com>
2023-06-30 13:39:56 +0200

    disabling the input and select elements  depending on the isLoading state

--- src/App.js
+++ src/App.js
@@ -37,16 +37,25 @@ function App() {
           type="text"
           value={amount}
           onChange={(e) => setAmount(Number(e.target.value))}
+          disabled={isLoading}
         />
 
-        <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
+        <select
+          value={fromCur}
+          onChange={(e) => setFromCur(e.target.value)}
+          disabled={isLoading}
+        >
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="CAD">CAD</option>
           <option value="INR">INR</option>
         </select>
 
-        <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
+        <select
+          value={toCur}
+          onChange={(e) => setToCur(e.target.value)}
+          disabled={isLoading}
+        >
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="CAD">CAD</option>
